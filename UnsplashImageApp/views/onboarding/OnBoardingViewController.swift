40cca1fdbd3b51f44edba66d982eebb29b18d2c7
Tavi Danner Manrique Nestarez <tmanriqu@LIM-FVFHL3GWQ05N.local>
2022-06-25 19:12:12 -0500

    instantiate the onboarding images

--- UnsplashImageApp/views/onboarding/OnBoardingViewController.swift
+++ UnsplashImageApp/views/onboarding/OnBoardingViewController.swift
@@ -18,6 +18,21 @@ class OnBoardingViewController: UIViewController {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        onBoardingPages = [
+            OnBoardingPage(
+                title: "Delicious Dishes",
+                description: "Experience a variety of amazing dishes from different cultures around the world.",
+                image: UIImage(named: "onboarding_image_1")!),
+            OnBoardingPage(
+                title: "World-Class Chefs",
+                description: "Our dishes are prepared by only the best.",
+                image: UIImage(named: "onboarding_image_2")!),
+            OnBoardingPage(
+                title: "Instant World-Wide Delivery",
+                description: "Your orders will be delivered instantly irrespective of your location around the world.",
+                image: UIImage(named: "onboarding_image_3")!)
+        ]
 
         // Do any additional setup after loading the view.
     }
