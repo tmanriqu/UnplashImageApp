7f7d03e2a654ba611ea95d80dea8a4a15bc75e46
Tavi Danner Manrique Nestarez <tmanriqu@LIM-FVFHL3GWQ05N.local>
2022-06-27 01:51:36 -0500

    add alamofire dependencies

--- UnsplashImageApp/views/main/photoscollection/PhotosCollectionViewController.swift
+++ UnsplashImageApp/views/main/photoscollection/PhotosCollectionViewController.swift
@@ -6,10 +6,37 @@
 //
 
 import UIKit
+import Alamofire
 
 class PhotosCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .orange
+        setUpNavigationBar()
+        setUpCollectionView()
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    private func setUpNavigationBar() {
+        let title = UILabel()
+        title.text = "PHOTOS"
+        title.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: title)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .link
+        return cell
     }
 }
